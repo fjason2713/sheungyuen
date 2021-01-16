aa4df04cb8597188f8f8358e59ca70b574895b5a
Jason Cheng <funnyjason27@gmail.com>
2021-01-16 14:32:00 +0800

    shop detail complete

--- src/components/InstagramFeed/InstagramFeed.tsx
+++ src/components/InstagramFeed/InstagramFeed.tsx
@@ -48,23 +48,21 @@ export default class InstagramFeed extends React.Component<IInstagramFeedProps, 
             })}
         </div>
         <div className={classes.shopOverview}>
-          <p className={classes.title}>
+          <p className={classes.header}>
             上弦閣 | 品牌故事
             <br />
             Luna Archaistic Jewellery | Background
           </p>
-          <div className={classes.largeLogo}>
-            <img src={SheungYuen} alt="SheungYuen" />
-          </div>
-          <p className={classes.content}>
-            上弦閣 | 品牌故事 Luna Archaistic Jewellery | Background 上弦閣 (Luna Archaistic Jewellery) 源自香港 🇭🇰
-            <br />
-            起初是一間 Instagram Store ，經營不到一年已經獲得不少熱愛古風的朋友關注
-            <br />
+          <img className={classes.largeLogo} src={SheungYuen} />
+          <p className={classes.body}>
+            <a href={'https://www.instagram.com/_sheungyuen_/?hl=zh-tw'} target={'_blank'}>
+              上弦閣 (Luna Archaistic Jewellery)
+            </a>
+            源自香港 🇭🇰， 起初是一間 Instagram Store ，經營不到一年已經獲得不少熱愛古風的朋友關注。
             我們的顧客來自香港、台灣和東南亞國家。 店主天凝 @_tin.ying_
             是一位漢服愛好者，上弦閣店內所有產品都是她親手設計和製作
             <br />
-            為了讓每一件飾品都有它獨特的故事，天凝也會特地戴上自製的古風首飾外出拍攝漢服影集
+            為了讓每一件飾品都有它獨特的故事，天凝也會特地戴上自製的古風首飾外出拍攝漢服影集。
           </p>
         </div>
         <div className={classes.products}></div>
